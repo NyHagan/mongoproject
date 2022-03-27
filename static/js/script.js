734ca81a5fae9e18a5299a2e7a7aa1ac4203db22
NyHagan <nanaohagan@gmail.com>
2022-03-27 15:21:15 +0000

    added

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,2 @@
-/*
-    jQuery for MaterializeCSS initialization
-*/
-
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-});
-
-
-
-
-
-/*
-    vanilla JavaScript for MaterializeCSS initialization
-*/
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     let sidenavs = document.querySelectorAll(".sidenav");
-//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
-// });
